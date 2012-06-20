5f652b50ed94faa158a12d3d98fb1f5006f0a06b
Ian Daniher <it.daniher@gmail.com>
2012-06-20 17:24:20 -0500

    turn findCompound.py into a basic utility

--- findCompound.py
+++ findCompound.py
@@ -1,8 +1,10 @@
 import pymongo
+import sys
+
 from pprint import pprint
 
 connection = pymongo.Connection('localhost')
 db = connection.chemicals
 
 findCompound = lambda regex: list(db.chebi.find({'$or':[{'ChEBI Name':regex},{'IUPAC Names':regex},{'Synonyms':regex}]}))
-pprint(findCompound("ethane")[0])
+pprint(findCompound(sys.argv[1])[0])
