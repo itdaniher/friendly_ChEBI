d657100a302445c8039354cd561c73ea5f3425f9
Ian Daniher <it.daniher@gmail.com>
2012-07-10 23:27:59 -0400

    nontrivial changes to the structure and cleanliness if the script, added compounds/*.json as an atomicization method, added new chemkit method to ensure minimal atom overlaps

--- postProcess.py
+++ postProcess.py
@@ -4,6 +4,7 @@ import json
 import gzip
 import re
 import random
+import os
 
 def lowerKeys(x):
 	""" convert all keys in the provided dictionary to lowercase """
@@ -15,8 +16,7 @@ def lowerKeys(x):
 
 
 def addImplicitHydrogens(molecule):
-	""" iterate through atoms in the molecule and add hydrogens 
-	to atoms not matching their expected valence """
+	""" iterate through atoms in the molecule and add hydrogens to atoms not matching their expected valence """
 	atoms = molecule.atoms()
 	for atom in atoms:
 		position = atom.neighbors()[0].position()
@@ -25,24 +25,29 @@ def addImplicitHydrogens(molecule):
 			hydrogen.setPosition(position.x()+random.random(), position.y()+random.random(), position.z()+random.random())
 			molecule.addBond(atom, hydrogen)
 
+def normalizeMolfile(molfile):
+	if type(molfile) in [str, unicode]:
+		molfile = molfile.splitlines()
+	count = [bool(re.search("^ *[0-9]+ *[0-9]+ .*", item)) for item in molfile].index(True)
+	if count < 3:
+		molfile.insert(0, '')
+		return normalizeMolfile(molfile)
+	elif count > 3:
+		molfile.pop(0)
+		return normalizeMolfile(molfile)
+	else:
+		molfile[0:3] = ['']*3
+		return '\n'.join(molfile)
+
+chunk = lambda l, x: [l[i:i+x] for i in xrange(0, len(l), x)]
+		
+
 def addInfo(compound):
 	""" process raw json element to chemjson and add molecular geometry """
-	chunk = lambda l, x: [l[i:i+x] for i in xrange(0, len(l), x)]
-	def normalizeMolfile(molfile):
-		if type(molfile) in [str, unicode]:
-			molfile = molfile.splitlines()
-		count = [bool(re.search("^ *[0-9]+ *[0-9]+ .*", item)) for item in molfile].index(True)
-		if count < 3:
-			molfile.insert(0, '')
-			return normalizeMolfile(molfile)
-		elif count > 3:
-			molfile.pop(0)
-			return normalizeMolfile(molfile)
-		else:
-			molfile[0:3] = ['']*3
-			return '\n'.join(molfile)
-			
 	compound = lowerKeys(compound)
+	if compound["chebi name"] + ".json" in os.listdir("./compounds"):
+		print compound["chebi name"] + " already processed!"
+		return json.loads(open("compounds/"+compound["chebi name"]+".json").read())
 	moleculeFile = chemkit.MoleculeFile()
 	moleculeFile.setFormat("sdf")
 	molfile = normalizeMolfile(compound["molfile"])
@@ -50,21 +55,19 @@ def addInfo(compound):
 	moleculeFile.setFormat("cjson")
 	if 'charge' not in compound.keys() or compound['charge'] == 0: 
 		addImplicitHydrogens(moleculeFile.molecule())
-	results = json.loads(moleculeFile.writeString())
-	print "starting", compound['chebi name'], '~', sum(results['atoms']['elements'])*2
+	print "starting", compound['chebi name'], '~', sum(json.loads(moleculeFile.writeString())['atoms']['elements'])*2
 	start = time.time()
+	chemkit.CoordinatePredictor.eliminateCloseContacts(moleculeFile.molecule())
 	chemkit.MoleculeGeometryOptimizer.optimizeCoordinates(moleculeFile.molecule())
-	results = json.loads(moleculeFile.writeString())
-	duration = time.time()-start
-	if 'name' in results.keys():
-		del results['name']
-	compound.update(results)
+	print '\t', round(time.time()-start, 4)
+	compound.update(json.loads(moleculeFile.writeString()))
 	del compound['molfile']
 	if 'formulae' in compound.keys():
 		del compound['formulae']
-	print '\t', round(duration, 4)
+	open("compounds/"+compound['chebi name']+".json", "w").write(json.dumps(compound))
 	return compound
 
 if __name__ == "__main__":
 	compounds = json.loads(gzip.open('ChEBI_complete.json.gz').read())
-	compounds = [addInfo(compound) for compound in compounds[0:5]]
+	compounds = [addInfo(compound) for compound in compounds]
+	gzip.open('ChEBI_complete.json.gz', 'w').write(json.dumps(compounds))
