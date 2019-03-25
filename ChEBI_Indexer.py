56eb83d229e60c1cdab2e3bfb15c2138c02364cc
itdaniher <itdaniher@gmail.com>
2019-03-24 22:08:08 -0400

    adjust schema, add error handling and docs, update README

--- ChEBI_Indexer.py
+++ ChEBI_Indexer.py
@@ -6,9 +6,15 @@ Options:
     --index PATH    Location of folder to store index. [default: index]
 """
 
-from whoosh.fields import Schema, TEXT, ID, STORED, NUMERIC
+import sqlite3 as sql
+import json
+import time
+import collections
+
+from whoosh.fields import Schema, TEXT, STORED, NUMERIC
 import whoosh.index as index
-import sys, mmap, json
+import sys
+import mmap
 import os.path
 from docopt import docopt as magic_docopt
 
@@ -16,12 +22,15 @@ magic_docopt()
 
 chebi = Schema(
     ChEBI=NUMERIC(int, 32, signed=False, stored=True),
-    InChI=TEXT(stored=True),
+    InChI=STORED,
+    InChIKey=TEXT(stored=True),
     SMILES=TEXT(stored=True),
     mass=NUMERIC(float, 32, stored=True),
     charge=NUMERIC(int, 32, signed=True, stored=True),
     formula=TEXT(stored=True),
-    name=TEXT(stored=True),
+    names=TEXT(stored=True),
+    CAS=TEXT(stored=True),
+    definition=STORED
 )
 
 if os.path.exists(arguments.index):
@@ -34,42 +43,13 @@ if not arguments.JSON_LINE_FILES:
     import glob
     arguments["JSON_LINE_FILES"] = glob.glob("chebi_split/*.json")
 
-writer = ix.writer(limitmb=512)
-
-
-json_keys = [
-    "Synonyms",
-    "ChEBI ID",
-    "SMILES",
-    "Star",
-    "Definition",
-    "Charge",
-    "KEGG COMPOUND Database Links",
-    "PubChem Database Links",
-    "Mass",
-    "IUPAC Names",
-    "ChEBI Name",
-    "InChIKey",
-    "IntEnz Database Links",
-    "Molfile",
-    "Rhea Database Links",
-    "Monoisotopic Mass",
-    "InChI",
-    "CAS Registry Numbers",
-    "Secondary ChEBI ID",
-    "Formulae",
-]
-
-import sqlite3 as sql
-import json
-import time
-import collections
+writer = ix.writer(procs=2, limitmb=512, multisegment=True)
 
 
 class ChEBIDatastore(object):
     def __init__(self, db_name="chebi_v0.db"):
         self.conn = sql.connect(db_name)
-        init_table = "CREATE TABLE IF NOT EXISTS chebi_table (ChEBI INT, name TEXT, mass REAL, charge INT, InChI TEXT, SMILES TEXT, formula TEXT)"
+        init_table = "CREATE TABLE IF NOT EXISTS chebi_table (ChEBI INT, names TEXT, mass REAL, charge INT, InChI TEXT, InChIKey TEXT, SMILES TEXT, formula TEXT, CAS TEXT, definition TEXT)"
         self.conn.execute(init_table)
         create_index = "CREATE INDEX IF NOT EXISTS chebi_index ON chebi_table (ChEBI ASC)"
         self.conn.execute(create_index)
@@ -87,9 +67,8 @@ class ChEBIDatastore(object):
         return [x for x in samples]
 
     def add_chebi(self, entity):
-        inserter = "INSERT INTO chebi_table VALUES(?, ?, ?, ?, ?, ?, ?)"
+        inserter = "INSERT INTO chebi_table VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         data = [x for x in entity.values()]
-        print(data)
         self.cursor.executemany(inserter, [data])
         return self.conn.commit()
 
@@ -99,29 +78,26 @@ if __name__ == "__main__":
     for JL_FILE in arguments.JSON_LINE_FILES:
         with open(JL_FILE, "r+") as f:
             data = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
-            i = 0
             while True:
                 line = data.readline()
                 if len(line.strip()) < 1:
                     break
                 res = json.loads(line.decode("utf-8"))
                 try:
-                    # assert res['SMILES'].lower().count('c') < 9
-                    out = collections.OrderedDict()
-                    out["ChEBI"] = int(res["ChEBI ID"].split(":")[-1])
-                    out["name"] = res["ChEBI Name"].lower()
+                    out = {}
+                    out["ChEBI"] = res["ChEBI ID"]
+                    out["names"] = "; ".join([res["ChEBI Name"], *res.get("IUPAC Names", []), *res.get("Synonyms", [])])
                     out["mass"] = res["Mass"]
                     out["charge"] = res["Charge"]
-                    out["InChI"] = res["InChI"]
-                    out["SMILES"] = res["SMILES"]
-                    out["formula"] = res["Formulae"]
-                    if type(out["formula"]) == list:
-                        out["formula"] = [x for x in out["formula"] if ("h2o" in x.lower()) or ("." in x)][0]
-                    i += 1
-                except Exception as e:
-                    out = None
-                    pass
-                if out != None:
+                    out["InChI"] = "".join(res.get("InChI", ""))
+                    out["InChIKey"] = "".join(res.get("InChIKey",""))
+                    out["SMILES"] = res["SMILES"][0]
+                    out["formula"] = " ".join(res.get("Formulae",[]))
+                    out["definition"] = res.get("Definition", "")
+                    out["CAS"] = " ".join(res.get("CAS Registry Numbers",[]))
                     writer.add_document(**out)
-                    datastore.add_chebi(out)
+                    if not datastore.get_chebi_id(out["ChEBI"]):
+                        datastore.add_chebi(out)
+                except:
+                    pass
     writer.commit()
--- search.py
+++ search.py
@@ -1,33 +1,50 @@
 """
 Usage:
+    search.py --field_help
     search.py [options] (KEYWORDS...)
 
 Options:
+    --search FIELD      Column to search. [default: names]
     --max_count COUNT   Maximum number of results to return. [default: 10]
-    --sort_by NAME      Sort results by this field name. Can be charge/formula/mass/name/InChI/SMILES [default: mass]
+    --sort_by FIELD     Sort results by this field name. [default: mass]
     --postfix_glob G    Use provided keyword as start of query. Appends G. [default: *]
     --prefix_glob G     Preface keyword with G for glob searching.
     --no_glob           Disables globbing.
     --index PATH        Specify path to the index made by ChEBI_Indexer.py. [default: index]
+    --field_help        Returns available fieldnames for searching and sorting.
 """
 from docopt import docopt as magic_docopt
 
 import sys
 import os
 
-from whoosh.index import open_dir
+from whoosh.fields import TEXT
 from whoosh.qparser import QueryParser, FuzzyTermPlugin
 
 magic_docopt()
+
+from whoosh.index import open_dir
 if not os.path.isdir(arguments.index):
     raise FileNotFoundError("can't find the ChEBI index")
 
-assert arguments.sort_by in "charge/formula/mass/name/InChI/SMILES".split("/")
-
 ix = open_dir(arguments.index)
 
+INDEXED_FIELDS = [field_name for field_name in ix.schema.names() if ix.schema[field_name].indexed]
+TEXT_FIELDS = [field_name for field_name in ix.schema.names() if isinstance(ix.schema[field_name], TEXT)]
+
+if arguments.field_help:
+    print(f"Sortable fields: {', '.join(INDEXED_FIELDS)}")
+    print(f"Searchable fields: {', '.join(TEXT_FIELDS)}")
+    quit()
+
+if arguments.sort_by not in INDEXED_FIELDS:
+    raise ValueError(f"Invalid sort field: {arguments.sort_by} not found.")
+
+if arguments.search not in TEXT_FIELDS:
+    raise ValueError(f"Invalid search field: {arguments.search} not found.")
+
 searcher = ix.searcher()
-parser = QueryParser("name", ix.schema)
+parser = QueryParser(arguments.search, ix.schema)
 keyword_sequence = " ".join(arguments.KEYWORDS)
 
 # no special commands
@@ -36,7 +53,7 @@ if keyword_sequence.islower() and all([keyword.isalpha() for keyword in keyword_
         keyword_sequence = ("*" if arguments.prefix_glob else "") + keyword_sequence + arguments.postfix_glob
 
 query = parser.parse(keyword_sequence)
-results = searcher.search(query, limit=int(arguments.max_count or 10), sortedby=arguments.sort_by)
+results = searcher.search(query, limit=int(arguments.max_count), sortedby=arguments.sort_by)
 print(results)
 for result in results:
     print(result)
