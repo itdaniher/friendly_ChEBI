dee5bf1b247079239fa9fe8303b2155cea822f19
itdaniher <itdaniher@gmail.com>
2019-03-24 22:05:04 -0400

    merge splitter into SDF parser

--- ChEBI_JSON_Builder.py
+++ ChEBI_JSON_Builder.py
@@ -4,20 +4,18 @@ Usage:
 
 Options:
     --input F    Input SDF file from EBI. [default: ChEBI_complete.sdf.gz]
-    --output F   Output JSON file. [default: ChEBI_complete.json]
+    --split_count COUNT     Number of lines per output file. [default: 5000]
+    --output_dir DIR        Output directory to place JSON line files in. [default: chebi_split]
 """
+import os
 import mmap
 import gzip
 import json
 import urllib
-import io
-
 from docopt import magic
 
 magic()
 
-
-## start parsing SDF
 
 def typeChemical(d):
     for key, value in d.items():
@@ -34,7 +32,8 @@ def typeChemical(d):
         return (d["ChEBI ID"], dict(sorted(d.items())))
 
 entries = []
-out_file = open(arguments.output, "w")
+os.makedirs(arguments.output_dir, mode=0o755, exist_ok=True)
+
 with open(arguments.input, "r") as f:
      mapped = mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ)
      gzfile = gzip.GzipFile(mode="r", fileobj=mapped)
@@ -59,6 +58,11 @@ with open(arguments.input, "r") as f:
         if done:
             break
 
+split_count = int(arguments.split_count or 5000)
+split_index = 0
 for entry in sorted(entries):
-    print(entry)
-    out_file.write(f"{entry[0]}\t{json.dumps(entry[1])}\n")
+    chebi_id, json_text = entry[0], json.dumps(entry[1])
+    if chebi_id > split_index:
+        f = open(f"{arguments.output_dir}/chebi-{split_index}+{split_count}.json", "w")
+        split_index += split_count
+    f.write(json_text.strip() + "\n")
